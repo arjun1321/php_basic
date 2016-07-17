f455f9de30247cea3ceee18b38d5bb6734882411
arjun1321 <guptaarjun152@gmail.com>
2016-07-17 19:47:17 +0530

    project final part

--- php_project_clickbait.php
+++ php_project_clickbait.php
@@ -1,52 +1,11 @@
 <?php 
     define("TITLE", "PHP Arrays");
+    include("function.php");
 
     if(isset( $_POST["fix_submit"] )) {
-        // grab value from text area in $_POST collection
-        // make all letters lowercase using strtolower() function
-        //store in a variable
-        $clickBait = strtolower( $_POST["clickbait_headline"] );
+        //call function
         
-        // store array of clickbait-sounding words or phrases
-        $a = array(
-                "scientists",
-                "doctors",
-                "hate",
-                "stupid",
-                "weird",
-                "simple",
-                "trick",
-                "shocked me",
-                "you'll never believe",
-                "hack",
-                "epic",
-                "unbelievable"
-            );
-        
-        // store array of replacement words or phrases
-        /* make sure each replacement is in the same order 
-           as the click bait word you're trying to replace */
-        
-        $b = array(
-                "so-called scientists",
-                "so-called doctors",
-                "aren't threatened by",
-                "average",
-                "completely normal",
-                "ineffective",
-                "method",
-                "is no different than the others",
-                "you won't really be surprised by",
-                "slightly improve",
-                "boring",
-                "normal"
-            );
-        
-        // use the str_replace() function to replace the words
-        // uppercase the first letter in every word using ucwords() function
-        // store in a variable
-        
-        $honestHeadline = str_replace( $a, $b, $clickBait );
+        checkForClickBait();
     }
 ?>
 
@@ -89,9 +48,18 @@
         
             if(isset( $_POST["fix_submit"] )) {
                 
-                // use ucwords() function to uppercase first letter in every word 
-                // echo the variable on the screen
-                echo "<strong class='text-danger'>Original Headline</strong><h4>".ucwords($clickBait)."</h4><hr>";
+                //get first value in array returned by checForClickBait() function
+                //store it in a variable
+                
+                $clickbait = checkForClickBait()[0];
+                
+                // get second value in array returned by checkForClickBait() function
+                // Store it in a variable
+                $honestHeadline = checkForClickBait()[1];
+                
+                // call function
+                // pass two arguments in the function
+                displayHonestHeadline($clickbait, $honestHeadline);
             }
         ?>
         
