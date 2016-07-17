50906286991de18d43b52780657f7c6ad56a3aaf
arjun1321 <guptaarjun152@gmail.com>
2016-07-17 15:04:36 +0530

    comment echo

--- index.php
+++ index.php
@@ -7,7 +7,32 @@
     
     <body>
         
+        <?php 
+            echo "Hello world!"
+        ?>
         
+        <!-- COMMENT -->
+        <?php
+            // single line comment 
+            # single line comment
+                /*
+                multi 
+                line
+                comment here
+                */
+        ?>
+        
+        <?php 
+            print('hello world!<br>');
+            print('hello world!<br>');  
+            PRINT('hello world!<br>');
+        
+        
+            $name = "Brad";
+            echo "Hello " . $name . "<br>";
+            echo "Hello " . $name . "<br>";
+            echo "Hello " . $NAME . "<br>";
+        ?>
         
     </body>
     
