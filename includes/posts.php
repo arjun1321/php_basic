87e23a5cf13c8c6b05c35d8e3e63c170fcb5b539
arjun1321 <guptaarjun152@gmail.com>
2016-07-18 14:13:43 +0530

    dynamic website using php

---
+++ includes/posts.php
@@ -0,0 +1,47 @@
+<?php 
+
+    // create array of blog posts
+
+    $posts = array(
+        
+        array(
+        
+            "post-title"   => "Sample blog post",
+            "post-date"    => "January 1, 2016",
+            "post-author"  => "Mark",
+            "post-content" => "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi aut, velit impedit sit delectus numquam tempora tenetur aspernatur itaque est asperiores dolore vitae recusandae ducimus a animi, libero. Quos, accusamus.</p>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate dicta, dolore voluptatem velit quidem deserunt incidunt similique pariatur! Vero similique quidem nostrum ullam, molestias nobis excepturi velit consectetur doloremque repellat! Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            
+            <p>Accusantium animi voluptas corporis incidunt, modi maxime dolorem ipsa quisquam ratione tempore accusamus facere quo ipsum consequuntur maiores, necessitatibus aperiam? Excepturi, explicabo!</P>"
+        ),
+        
+        array(
+        
+            "post-title"   => "Another blog post",
+            "post-date"    => "July 1, 2016",
+            "post-author"  => "Anuj",
+            "post-content" => "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi aut, velit impedit sit delectus numquam tempora tenetur aspernatur itaque est asperiores dolore vitae recusandae ducimus a animi, libero. Quos, accusamus.</p>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate dicta, dolore voluptatem velit quidem deserunt incidunt similique pariatur! Vero similique quidem nostrum ullam, molestias nobis excepturi velit consectetur doloremque repellat! Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            
+            <p>Accusantium animi voluptas corporis incidunt, modi maxime dolorem ipsa quisquam ratione tempore accusamus facere quo ipsum consequuntur maiores, necessitatibus aperiam? Excepturi, explicabo!</P>"
+        ),
+        
+        array(
+        
+            "post-title"   => "New feature",
+            "post-date"    => "August 1, 2016",
+            "post-author"  => "Roshan",
+            "post-content" => "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi aut, velit impedit sit delectus numquam tempora tenetur aspernatur itaque est asperiores dolore vitae recusandae ducimus a animi, libero. Quos, accusamus.</p>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate dicta, dolore voluptatem velit quidem deserunt incidunt similique pariatur! Vero similique quidem nostrum ullam, molestias nobis excepturi velit consectetur doloremque repellat! Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            
+            <p>Accusantium animi voluptas corporis incidunt, modi maxime dolorem ipsa quisquam ratione tempore accusamus facere quo ipsum consequuntur maiores, necessitatibus aperiam? Excepturi, explicabo!</P>"
+        )
+    
+    );
+
+?>
+
+
