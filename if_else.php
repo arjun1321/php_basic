55f4ddb9cf0881517b9c17f53aac28a86effd2c8
arjun1321 <guptaarjun152@gmail.com>
2016-07-18 11:49:27 +0530

    if else

---
+++ if_else.php
@@ -0,0 +1,69 @@
+<?php 
+    define("TITLE", "if, else &amp; elseif statement");
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title><?php echo TTILE; ?></title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <div class="container">
+        <h1><?php echo TITLE; ?></h1>
+        
+        <?php
+            // If expression is TRUE, do something
+            // If expression is FALSE, don't do anything
+        
+            $foo = 99;
+            $bar = 109;
+    
+            
+            if( $foo > $bar ) {
+                // code to execute if true
+                
+                echo "$foo is greater than $bar";
+            } else if($foo == $bar) {
+                // if false
+                echo "$bar is equal to $foo";
+            } else {
+                echo "$bar is greater than $foo";
+            }
+        
+            
+        ?>
+        
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+</html>
